8513594154c8f288a23cfbaa0cd1729b9ff1291a
stoller <stoller@stollerde-MacBook-Pro.local>
2018-04-08 23:22:12 +0800

    ebook_final

---
+++ xcodeapp02/TrueLoveViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TrueLoveViewController.swift
+//  xcodeapp02
+//
+//  Created by stoller on 2018/3/24.
+//  Copyright © 2018年 hung. All rights reserved.
+//
+
+import UIKit
+
+class TrueLoveViewController: UIViewController {
+
+    @IBAction func testbutton(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var FtextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var Fsegment: UISegmentedControl!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+}
+
+
